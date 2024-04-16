8ad331a36a8d140cdad1f849c48b8ef5f52cc899
HeitorFernandes <heitorsoaresfer@gmail.com>
2024-04-16 14:41:11 -0300

    refactor states no Provider

--- src/context/Provider.js
+++ src/context/Provider.js
@@ -1,10 +1,30 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
 import AppContext from './AppContext';
 
-function Provider({ children }) { 
-    
+function Provider({ children }) {
+    const [catList, setCatList] = React.useState([]);
+    const [tags, setTag] = React.useState([]);
+    const [formData, setFormData] = React.useState({
+        email: { value: '', error: '' },
+        password: { value: '', error: '' },
+        confirmPassword: { value: '', error: '' },
+        name: { value: '', error: '' },
+        surname: { value: '', error: '' },
+        dob: { value: '', error: '' },
+        streetNumber: { value: '', error: '' },
+        city: { value: '', error: '' },
+        state: { value: '', error: '' },
+        postalCode: { value: '', error: '' },
+     });
+
     const context = {
+        setCatList,
+        catList,
+        setTag,
+        tags,
+        formData,
+        setFormData,
     };
 
     return (
