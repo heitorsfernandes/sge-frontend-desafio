41a7f14d09ff0cb6482def666d82bef4c1df55a4
HeitorFernandes <heitorsoaresfer@gmail.com>
2024-04-16 16:06:36 -0300

    add images, mock e bibliotecas

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import { Routes, Route, Navigate } from 'react-router-dom';
 import React from 'react';
 import Provider from './context/Provider';
+import CatList from './pages/CatList';
+import Form from './pages/Form';
 
 function App() {
   return (
   <Provider>
       <Routes>
         <Route path="/" element={<Navigate to="/catList" replace />} />
-        <Route path="/catList"/>
-        <Route path="/form"/>
+        <Route path="/catList" element={< CatList />}/>
+        <Route path="/form" element={< Form />}/>
       </Routes>
   </Provider>
   );
